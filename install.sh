2f4f2c6eaca004a8e6fc806c8eb3bbf5d837aa4a
Tilo Spannagel <development@tilosp.de>
2020-09-02 19:44:55 +0200

    Add git config

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+DOTFILES="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+git config --global --replace-all include.path "$DOTFILES/git/main.ini" "^$DOTFILES/git/main.ini$"
+git config --global core.excludesfile "$DOTFILES/git/.gitignore"
